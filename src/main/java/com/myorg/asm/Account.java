036a100ad443db0beaab1ad598397ea12e8c3e4b
huyan <huyan@cd.tuan800.com>
2015-09-25 18:00:34 +0800

    asm 生成子类

--- src/main/java/com/myorg/asm/Account.java
+++ src/main/java/com/myorg/asm/Account.java
@@ -8,4 +8,18 @@ public class Account {
     public void operate(){
         System.out.println("account operate");
     }
+
+    public void operate2(int a){
+        System.out.println("operate2");
+    }
+
+    private int methodReturnInt(){
+        System.out.println("methodReturnInt");
+        return 1;
+    }
+
+    protected String methodString(int a, String tss, SecurityChecker securityChecker){
+        System.out.println("methodString is run");
+        return "";
+    }
 }
