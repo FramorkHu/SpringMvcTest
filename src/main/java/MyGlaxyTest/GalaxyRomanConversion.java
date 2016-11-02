5bca135bb7bead65194759f11419fce644a4b3c7
huyan <475616043@qq.com>
2016-11-02 18:00:29 +0800

    up

---
+++ src/main/java/MyGlaxyTest/GalaxyRomanConversion.java
@@ -0,0 +1,15 @@
+package MyGlaxyTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by huyan on 2016/11/2.
+ */
+public class GalaxyRomanConversion {
+
+    Map<String , Double> creditsInfoMap = new HashMap<>();
+    Map<String, String> galaxyRomanValueMap = new HashMap<>();
+
+
+}
